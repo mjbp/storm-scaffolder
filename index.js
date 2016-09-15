25cc37e0fc3050c6e72aa4a75f85368f9b9251d4
mjbp <mick.perkins@stormid.com>
2016-09-15 18:11:09 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,43 @@
+var Promise = require('bluebird'),
+    inquirer = require('inquirer'),
+    exec = require('child_process').exec,
+    execP = Promise.promisify(require('child_process').exec),
+    files = require('./libs/files'),
+    chalk = require('chalk'),
+    clear = require('clear'),
+    figlet  = require('figlet');
+
+function run(callback) {
+  var questions = [
+    {
+      name: 'project',
+      type: 'input',
+      message: 'Enter your project name:',
+      validate: function( value ) {
+        if (value.length) {
+          return true;
+        } else {
+          return 'Please enter a project name';
+        }
+      }
+    }
+  ];
+
+  inquirer.prompt(questions).then(callback);
+}
+
+clear();
+console.log(
+  chalk.yellow(
+    figlet.textSync('StormID', { horizontalLayout: 'full' })
+  )
+);
+
+run(function(answer){
+    console.log('Creating your new project, this may take some time...');
+    exec(["git clone git@github.com:mjbp/storm-scaffold.git && mv 'storm-scaffold' '",
+                answer.project.split(' ').join('-').toLowerCase(),
+                "' && cd ./",
+                answer.project.split(' ').join('-').toLowerCase(),
+                "&& echo 'installing dependencies...' && npm i"].join(''));
+});
